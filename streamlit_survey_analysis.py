30e03eb627e6055ce9b605600f65578f73d2e939
Kunal Khadilkar <kunalkhadilkar@gmail.com>
2021-03-18 15:22:38 -0400

    Add files via upload

--- streamlit_survey_analysis.py
+++ streamlit_survey_analysis.py
@@ -5,80 +5,306 @@ import numpy as np
 import pandas as pd
 import altair as alt
 import plotly.express as px
+import plotly.graph_objects as go
 st.image('CSL Logo.png',width=500)
 st.title('Giant Eagle Survey Analysis')
-df = pd.read_csv('survey_data.csv')
+dataset = pd.read_csv('survey_data.csv')
 
+df = dataset
 df['Which university do you belong to?'] = df['Which university do you belong to?'].replace(['Carnegie Mellon','Carnegie Mellon University','carnegie mellon','School of Computer Science','Carnegie mellon university','Carnegie Mellon University, School of Computer Science','School of Computer Science','Cmu','Carnegie Mellon University - CIT','Carnegie Mellon Univsersity','Carnegie Mellon Unviersity','Carnegie Mellon University','CIT','Carnegie Mellon ','Carnegie Mellon University ','CMU'],'Carnegie Mellon University')
 df['Which university do you belong to?'] = df['Which university do you belong to?'].replace(['University of Pittsburgh','University of Pittsburgh '],'University of Pittsburgh')
 df['How often do you eat out? (Including dine-in, take outs, and deliveries)'] = df['How often do you eat out? (Including dine-in, take outs, and deliveries)'].replace(["Last semester was 5x a week (which I hated because I prefer cooking for health, but I didn't have time). Now 1-2x a month"],'1-3 times a month')
+df['Select the type of kitchen you have access to in your residence'] = df['Select the type of kitchen you have access to in your residence'].replace(["Well, it has a cooker. We have a fridge and air extractor. So, I don't know if that counts as full."],'It has a small kitchen or kitchenette (e.g. small stove, fridge, and etc.).')
+
 print(df.columns)
 value_counts_series = df['Which university do you belong to?'].value_counts()
 uni_counts = value_counts_series.tolist()
 uni_names = value_counts_series.index.tolist()
-if st.checkbox('Raw Data'):
-    st.write(df[0:5])
+
+st.sidebar.title("Select Analysis")
+st.sidebar.markdown("Choose between survey or interview analysis:")
+option = st.sidebar.radio("Choose:",   ('Survey Analysis', 'Interview Analysis'))
+
+if option=='Survey Analysis':
+	if st.checkbox('Raw Data'):
+		st.write(df[0:5])
+
+	st.write('Basic Demographics')
+
+	expander_1 = st.beta_expander("Who is answering our survey?")
+	with expander_1:
+		st.title('Who is answering our Survey? :bar_chart:')
+
+		source = pd.DataFrame({
+			'Universities': uni_names,
+			'Number of Respondents': uni_counts
+		})
+
+		a = alt.Chart(source,width=500).mark_bar(size=40).encode(
+			x='Universities',
+			y='Number of Respondents'
+		)
+
+		st.altair_chart(alt.layer(a))
+
+	# add_selectbox = st.sidebar.selectbox(
+	#     "How would you like to be contacted?",
+	#     ("Email", "Home phone", "Mobile phone")
+	# )
+	expander_2 = st.beta_expander("Year wise distribution")
+	with expander_2:
+		st.title('Year wise distribution :school_satchel:')
+		# This dataframe has 244 lines, but 4 distinct values for `day` 
+		year_series = df['Which best describes you?'].value_counts()
+		year_values = year_series.tolist()
+		year_names = year_series.index.tolist()
+		fig = px.pie(values=year_values, names=year_names)
+		st.plotly_chart(fig)
+
+	expander_3 = st.beta_expander("Are you an international student?")
+	with expander_3:
+		st.title('Are you an international student? :school:')
+		# This dataframe has 244 lines, but 4 distinct values for `day` 
+		international_series = df['Are you an international student?'].value_counts()
+		international_values = international_series.tolist()
+		international_names = international_series.index.tolist()
+		fig = px.pie(values=international_values, names=international_names)
+		st.plotly_chart(fig)
+
+	st.write("----------------------------------------------------------------------------------------------------")
+	st.write('Kitchen Amenities and Eating Habits')
+
+	expander_4 = st.beta_expander("Select the type of kitchen you have access to in your residence")
+	with expander_4:
+		st.title('Select the type of kitchen you have access to in your residence')
+		# This dataframe has 244 lines, but 4 distinct values for `day` 
+		kitchen_series = df['Select the type of kitchen you have access to in your residence'].value_counts()
+		kitchen_values = kitchen_series.tolist()
+		kitchen_names = kitchen_series.index.tolist()
+		kitchen_df = pd.DataFrame({
+			'Type of Kitchen': kitchen_names,
+			'Number of Respondents': kitchen_values
+		})
+		fig = px.bar(kitchen_df,y='Number of Respondents', x='Type of Kitchen',height=500)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
 
 
-st.echo('Survey Analysis')
+	expander_5 = st.beta_expander("How often do you eat out? (Including dine-in, take outs, and deliveries)")
+	with expander_5:
+		st.title('How often do you eat out? (Including dine-in, take outs, and deliveries) :fries:')
+		# This dataframe has 244 lines, but 4 distinct values for `day` 
+		eating_series = df['How often do you eat out? (Including dine-in, take outs, and deliveries)'].value_counts()
+		eating_values = eating_series.tolist()
+		eating_names = eating_series.index.tolist()
+		eating_df = pd.DataFrame({
+			'Eating Habits': eating_names,
+			'Number of Respondents': eating_values
+		})
+		fig = px.bar(eating_df,y='Number of Respondents', x='Eating Habits',height=500)
 
-expander_1 = st.beta_expander("Who is answering our survey?")
-with expander_1:
-	st.title('Who is answering our Survey? :bar_chart:')
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
 
-	source = pd.DataFrame({
-	    'Universities': uni_names,
-	    'Number of Respondents': uni_counts
-	})
+	expander_6 = st.beta_expander("How much do you spend on an average per month for grocery shopping?")
+	with expander_6:
+		st.title('How much do you spend on an average per month for grocery shopping?')
+		# This dataframe has 244 lines, but 4 distinct values for `day` 
+		money_series = df['How much do you spend on an average per month for grocery shopping?'].value_counts()
+		money_values = money_series.tolist()
+		money_names = money_series.index.tolist()
+		money_df = pd.DataFrame({
+			'Amount Spent per month on groceries': money_names,
+			'Number of Respondents': money_values
+		})
+		fig = px.bar(money_df,y='Number of Respondents', x='Amount Spent per month on groceries',height=500)
 
-	a = alt.Chart(source,width=500).mark_bar(size=40).encode(
-	    x='Universities',
-	    y='Number of Respondents'
-	)
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
 
-	st.altair_chart(alt.layer(a))
+	expander_7 = st.beta_expander("The most important factors in determining a grocery store")
+	with expander_7:
+		st.title('Most important factors in deciding the grocery store')
+		# This dataframe has 244 lines, but 4 distinct values for `day` 
+		factor_series = df['Rank the most important factor in deciding your grocery store [One]'].value_counts()
+		factor_values = factor_series.tolist()
+		factor_names = factor_series.index.tolist()
+		factor_df = pd.DataFrame({
+			'Most Important Factor': factor_names,
+			'Number of Respondents': factor_values
+		})
+		print(factor_df.head())
+		fig = go.Figure(data=[go.Pie(labels=factor_names, values=factor_values, hole=.3)])
 
-# add_selectbox = st.sidebar.selectbox(
-#     "How would you like to be contacted?",
-#     ("Email", "Home phone", "Mobile phone")
-# )
-expander_2 = st.beta_expander("Year wise distribution")
-with expander_2:
-	st.title('Year wise distribution :school_satchel:')
-	# This dataframe has 244 lines, but 4 distinct values for `day` 
-	year_series = df['Which best describes you?'].value_counts()
-	year_values = year_series.tolist()
-	year_names = year_series.index.tolist()
-	fig = px.pie(values=year_values, names=year_names)
-	st.plotly_chart(fig)
+		#fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
 
-expander_3 = st.beta_expander("Are you an international student?")
-with expander_3:
-	st.title('Are you an international student? :school:')
-	# This dataframe has 244 lines, but 4 distinct values for `day` 
-	international_series = df['Are you an international student?'].value_counts()
-	international_values = international_series.tolist()
-	international_names = international_series.index.tolist()
-	fig = px.pie(values=international_values, names=international_names)
-	st.plotly_chart(fig)
 
-expander_4 = st.beta_expander("How often do you eat out? (Including dine-in, take outs, and deliveries)")
-with expander_4:
-	st.title('How often do you eat out? (Including dine-in, take outs, and deliveries) :fries:')
-	# This dataframe has 244 lines, but 4 distinct values for `day` 
-	eating_series = df['How often do you eat out? (Including dine-in, take outs, and deliveries)'].value_counts()
-	eating_values = eating_series.tolist()
-	eating_names = eating_series.index.tolist()
-	eating_df = pd.DataFrame({
-	    'Eating Habits': eating_names,
-	    'Number of Respondents': eating_values
-	})
-	fig = px.bar(eating_df,y='Number of Respondents', x='Eating Habits',height=500)
+		factor_series = df['Rank the most important factor in deciding your grocery store [Two]'].value_counts()
+		factor_values = factor_series.tolist()
+		factor_names = factor_series.index.tolist()
+		factor_df = pd.DataFrame({
+			'Most Important Factor': factor_names,
+			'Number of Respondents': factor_values
+		})
+		print(factor_df.head())
+		fig = go.Figure(data=[go.Pie(labels=factor_names, values=factor_values, hole=.3)])
 
-	fig.update_yaxes(automargin=True)
-	st.plotly_chart(fig)
+		#fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
 
-# left_column, right_column = st.beta_columns(2)
-# pressed = left_column.button('Press me?')
-# if pressed:
-#     right_column.write("Woohoo!")
+		factor_series = df['Rank the most important factor in deciding your grocery store [Three]'].value_counts()
+		factor_values = factor_series.tolist()
+		factor_names = factor_series.index.tolist()
+		factor_df = pd.DataFrame({
+			'Most Important Factor': factor_names,
+			'Number of Respondents': factor_values
+		})
+
+		print(factor_names)
+		factor_names = ['Quality','Proximity','Product variety','Promotions', 'Open Hours']
+		factor_values = [10,16,19,2,5]
+		fig = go.Figure(data=[go.Pie(labels=factor_names, values=factor_values, hole=.3,sort=False)])
+
+		#fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+	expander_8 = st.beta_expander("Grocery Store Preferences")
+	with expander_8:
+		st.title('Grocery Store Preferences')
+		# This dataframe has 244 lines, but 4 distinct values for `day` 
+		store_series = df['Select your favorite grocery shopping choices [First choice]'].value_counts()
+		store_values = store_series.tolist()
+		store_names = store_series.index.tolist()
+		store_df = pd.DataFrame({
+			'First Preferred Store': store_names,
+			'Number of Respondents': store_values
+		})
+		fig = px.bar(store_df,y='Number of Respondents', x='First Preferred Store',height=500)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+		store_series = df['Select your favorite grocery shopping choices [Second choice]'].value_counts()
+		store_values = store_series.tolist()
+		store_names = store_series.index.tolist()
+		store_df = pd.DataFrame({
+			'Second Preferred Store': store_names,
+			'Number of Respondents': store_values
+		})
+		fig = px.bar(store_df,y='Number of Respondents', x='Second Preferred Store',height=500)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+		store_series = df['Select your favorite grocery shopping choices [Third choice]'].value_counts()
+		store_values = store_series.tolist()
+		store_names = store_series.index.tolist()
+		store_df = pd.DataFrame({
+			'Third Preferred Store': store_names,
+			'Number of Respondents': store_values
+		})
+		fig = px.bar(store_df,y='Number of Respondents', x='Third Preferred Store',height=500)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+
+if option=='Interview Analysis':
+	interview_dataset = pd.read_csv('interview_data.csv')
+	print(interview_dataset.head())
+	int_df = interview_dataset
+
+	if st.checkbox('Raw Data'):
+		st.write(interview_dataset[0:5])
+
+	expander = st.beta_expander("Basic Demographics")
+	with expander:
+		st.title('Gender')
+			# This dataframe has 244 lines, but 4 distinct values for `day` 
+		demographic_series = int_df['Gender'].value_counts()
+		demographic_values = demographic_series.tolist()
+		demographic_names = demographic_series.index.tolist()
+		demographic_df = pd.DataFrame({
+				'Gender': demographic_names,
+				'Number of Respondents': demographic_values
+		})
+		fig = px.pie(values=demographic_values, names=demographic_names)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+		st.title('University Year')
+			# This dataframe has 244 lines, but 4 distinct values for `day` 
+		demographic_series = int_df['Class'].value_counts()
+		demographic_values = demographic_series.tolist()
+		demographic_names = demographic_series.index.tolist()
+		demographic_df = pd.DataFrame({
+				'Class': demographic_names,
+				'Number of Respondents': demographic_values
+		})
+		fig = px.pie(values=demographic_values, names=demographic_names)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+	expander = st.beta_expander("Housing Details")
+	with expander:
+		st.title('Do the students live on campus?')
+			# This dataframe has 244 lines, but 4 distinct values for `day` 
+		demographic_series = int_df['Live on campus'].value_counts()
+		demographic_values = demographic_series.tolist()
+		demographic_names = demographic_series.index.tolist()
+		demographic_df = pd.DataFrame({
+				'Live on campus': demographic_names,
+				'Number of Respondents': demographic_values
+		})
+		fig = px.pie(values=demographic_values, names=demographic_names)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+		st.title('Do the students live alone?')
+			# This dataframe has 244 lines, but 4 distinct values for `day` 
+		demographic_series = int_df['Live alone'].value_counts()
+		demographic_values = demographic_series.tolist()
+		demographic_names = demographic_series.index.tolist()
+		demographic_df = pd.DataFrame({
+				'Live on campus': demographic_names,
+				'Number of Respondents': demographic_values
+		})
+		fig = px.pie(values=demographic_values, names=demographic_names)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+	expander = st.beta_expander("Money Spent on Groceries and Takeouts")
+	with expander:
+		st.title('How much do students spend on groceries each week?')
+			# This dataframe has 244 lines, but 4 distinct values for `day` 
+		demographic_series = int_df['Spent on Groceries Range'].value_counts()
+		demographic_values = demographic_series.tolist()
+		demographic_names = demographic_series.index.tolist()
+		demographic_df = pd.DataFrame({
+				'Money Spent on Groceries': demographic_names,
+				'Number of Respondents': demographic_values
+		})
+		fig = px.bar(demographic_df,y='Number of Respondents', x='Money Spent on Groceries',height=500)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
+
+		st.title('How much do students spend on takeouts each week?')
+			# This dataframe has 244 lines, but 4 distinct values for `day` 
+		demographic_series = int_df['Takeout Range'].value_counts()
+		demographic_values = demographic_series.tolist()
+		demographic_names = demographic_series.index.tolist()
+		demographic_df = pd.DataFrame({
+				'Money Spent on Takeouts': demographic_names,
+				'Number of Respondents': demographic_values
+		})
+		fig = px.bar(demographic_df,y='Number of Respondents', x='Money Spent on Takeouts',height=500)
+
+		fig.update_yaxes(automargin=True)
+		st.plotly_chart(fig)
